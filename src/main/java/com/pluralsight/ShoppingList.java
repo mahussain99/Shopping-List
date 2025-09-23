873d0a41dbbce6257f7a58d86dd5338cf18751d3
Family <mh5212357@gmail.com>
2025-09-23 10:25:20 -0400

    Here is my Shopping List

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("1. Mango");
+        System.out.println("2. Apple");
+        System.out.println("3. Dell Laptop");
+        System.out.println("4. Rice");
+        System.out.println("5. Salt");
+        System.out.println("6. Chicken");
+        System.out.println("7. Fish");
+        System.out.println("8. Vegetable");
+        System.out.println("9. Oil");
+        System.out.println("10. Lentil");
+
+
+    }
+}
